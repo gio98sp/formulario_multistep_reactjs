2f3dc05167c6b809be578d7bcd842df9b511481f
Giovane Santos <giovane98sp@gmail.com>
2023-08-14 10:48:15 -0300

    add back and next form function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
+import { FiSend } from 'react-icons/fi';
 
 import './App.css';
 
@@ -10,29 +11,44 @@ import { useForm } from './hooks/useForm';
 export const App = () => {
   const formComponents = [<UseForm />, <ReviewForm />, <Thanks />];
 
-  const {currentStep, currentComponent} = useForm(formComponents)
+  const { currentStep, currentComponent, changeStep, isFirstStep, isLastStep } =
+    useForm(formComponents);
 
   return (
     <div className="App">
       <div className="header">
         <h2>Deixe sua avaliação</h2>
+
         <p>
           Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto
         </p>
       </div>
+
       <div className="form-container">
         <p>Etapas</p>
-        <form>
+
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
+
           <div className="actions">
-            <button type="button">
-              <GrFormPrevious />
-              <span>Voltar</span>
-            </button>
-            <button type="submit">
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
+            {!isFirstStep && (
+              <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+            )}
+
+            {!isLastStep ? (
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
           </div>
         </form>
       </div>
--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -3,8 +3,22 @@ import { useState } from 'react';
 export const useForm = (steps) => {
   const [currentStep, setCurrentStep] = useState(0);
 
+  const changeStep = (index, event) => {
+    if (event) event.preventDefault();
+
+    if (index < 0 || index >= steps.length) return;
+
+    setCurrentStep(index);
+  };
+
+  const isFirstStep = currentStep === 0
+  const isLastStep = currentStep + 1 === steps.length;
+
   return {
     currentStep,
     currentComponent: steps[currentStep],
+    changeStep,
+    isFirstStep,
+    isLastStep
   };
 };
