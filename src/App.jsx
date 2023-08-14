16f5133fdc2347ebd1a3961c632c191a0e566795
Giovane Santos <giovane98sp@gmail.com>
2023-08-14 09:13:42 -0300

    create initial structure form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css'
+import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
+
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <h1>form</h1>
-    </>
-  )
+    <div className="App">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>Etapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type="button">
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
