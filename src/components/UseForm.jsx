b91e36b240e72c51afc2c72f5fb9472cfbc94be7
Giovane Santos <giovane98sp@gmail.com>
2023-08-14 11:10:17 -0300

    create UseForm and styles

--- src/components/UseForm.jsx
+++ src/components/UseForm.jsx
@@ -1,7 +1,15 @@
-
 export const UseForm = () => {
-
   return (
-    <h1>UseForm</h1>
-  )
-}
+    <div>
+      <div className="form-control">
+        <label htmlFor="name">Nome:</label>
+        <input type="text" name="name" id="name" placeholder="Digite seu nome" required />
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="name">Email:</label>
+        <input type="email" name="email" id="email" placeholder="Digite seu Email" required />
+      </div>
+    </div>
+  );
+};
